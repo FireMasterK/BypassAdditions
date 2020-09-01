65d6b420fba06f098e106c33ab21b25f65655bca
FireMasterK <20838718+FireMasterK@users.noreply.github.com>
2020-09-01 17:34:45 +0530

    Time for some fun :)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BypassAdditions
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.4.1
 // @updateURL    https://raw.githubusercontent.com/FireMasterK/BypassAdditions/master/script.js
 // @description  Bypass links that cannot be bypassed by Universal Bypass
 // @author       FireMasterK
@@ -14,35 +14,72 @@
 var url = window.location.href.toString();
 
 if (url.indexOf("?r=") != -1) {
- window.location = atob(decodeURIComponent(url.substr(url.indexOf("?r=") + 3)));
+  window.location = atob(decodeURIComponent(url.substr(url.indexOf("?r=") + 3)));
 } else {
-  let o = {
-  timestamp: new Date().getTime(),
-  random: (Math.floor(Math.random() * 10000000)).toString()
- };
- var bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link/static" + window.location.pathname;
- GM.xmlHttpRequest({
-  method: "GET",
-  headers: {
-   "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
-  },
-  url: bypass_url,
-  onload: function(response) {
-   var json = JSON.parse(response.responseText);
-   o.link_id = json.data.link.id
-   bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/target?serial=" + btoa(JSON.stringify(o));
 
-    GM.xmlHttpRequest({
+  // iframe check
+  if (window.parent.location != window.location) { return }
+
+  GM.xmlHttpRequest({
     method: "GET",
     headers: {
-     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+    },
+    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/captcha",
+    onload: function () { }
+  });
+  GM.xmlHttpRequest({
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+    },
+    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/countdown_impression?trafficOrigin=network",
+    onload: function () { }
+  });
+  GM.xmlHttpRequest({
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+    },
+    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/todo_impression?mobile=true&trafficOrigin=network",
+    onload: function () { }
+  });
+  GM.xmlHttpRequest({
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+    },
+    url: "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/click?trafficOrigin=network",
+    onload: function () { }
+  });
+
+  let o = {
+    timestamp: new Date().getTime(),
+    random: "6548307"
+  };
+  var bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link/static" + window.location.pathname;
+  GM.xmlHttpRequest({
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
     },
     url: bypass_url,
-    onload: function(response) {
-     var json = JSON.parse(response.responseText);
-     window.location = json.data.target;
+    onload: function (response) {
+      var json = JSON.parse(response.responseText);
+      o.link_id = json.data.link.id
+      bypass_url = "https://publisher.linkvertise.com/api/v1/redirect/link" + window.location.pathname + "/target?serial=" + encodeURIComponent(btoa(JSON.stringify(o)));
+
+      GM.xmlHttpRequest({
+        method: "GET",
+        headers: {
+          "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Mobile/15E148 Safari/604.1"
+        },
+        url: bypass_url,
+        onload: function (response) {
+          var json = JSON.parse(response.responseText);
+          window.location = json.data.target;
+        }
+      });
     }
-   });
-  }
- });
+  });
 }
